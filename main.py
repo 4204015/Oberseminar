8dd004989ec77f62150754ddce6fdcaa466f601c
4204015 <4204015@gmail.com>
2016-11-21 21:09:43 +0100

    PRBS Generator -> neu, Kreuzkorrelationsmethode -> begonnen

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ Oberseminar Regelungstechnik - Auto-tuning PID
 from pyblocksim import inputs, TFBlock, stepfnc, blocksimulation, compute_block_ouptputs
 
 # Plotting library
-from matplotlib.pyplot import plot, grid, show
+from matplotlib.pyplot import plot, grid, show, figure
 
 # Library for symbolic mathematics
 import sympy as sp
@@ -24,6 +24,7 @@ import numpy as np
 from sim import Simulator
 from area_method import area_method
 from prbs_gen import prbsfnc
+from cross_cor_method import cross_cor_method
 
 # SYSTEM ----------------------------------------------------------------------
 
@@ -34,40 +35,47 @@ K = 1
 T = 3
 PT1 = K / (T*s + 1)
 
+#PT2
+K = 1
+T = 3
+theta = 0.3
+PT2 = K / (T**2 * s**2 + 2*theta*T*s + 1)
 
 # SIMULATIONSSTEUERUNG --------------------------------------------------------
 
-t_max = 0.1                  # Simulationsdauer in Sekunden
+t_max = 50                  # Simulationsdauer in Sekunden
 
 t_sprung = 0
-#u = stepfnc(t_sprung, 45)    # Eingangsfunktion mit Zeitpunkt, Sprunghöhe
+#u = stepfnc(t_sprung, 1)  # Eingangsfunktion mit Zeitpunkt, Sprunghöhe
 
-u = prbsfnc(10,10)
+ufnc, u, A, N = prbsfnc(50,2**10)        # PRBS Signal mit Amplitude, Periodendauer
 
 PID = [3,1,1,5]             # Parameter des PID Reglers - T_i, T_d, T_n, K
-t, b_out, G = Simulator(t_max,u,PT1,True,*PID)
+t, b_out, G = Simulator(t_max,ufnc,PT1,True,*PID)
 y = b_out[G]
 
 # SYSTEMIDENTIFIKATION --------------------------------------------------------
 
-System = 'PT1'
+System = ''
 
 # PARAMETERIDENTIFIKATION -----------------------------------------------------
 
 if System == 'PT1':
-
     K_e,T_e = area_method(t,t_sprung,b_out[G])
+    print("K_e = " + str(K_e))
+    print("T_e = " + str(T_e))
     
 #elif System == 'PT2':
     
     
-#else:   # System -> unknown
-    
+else:   # System -> unknown
+    F = cross_cor_method(b_out[G],u,A,N)
+    figure(0)
+    plot(F[1:100].real,F[1:100].imag)
+     
 
 # AUSGABE ---------------------------------------------------------------------
-
-print("K_e = " + str(K_e))
-print("T_e = " + str(T_e))
+figure(1)
 plot(t, b_out[G])
 grid()
 show()
